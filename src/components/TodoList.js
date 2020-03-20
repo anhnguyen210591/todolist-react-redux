9a6368de92c7626294cbdaef35fcbf0c7243cb06
MAC <anhnguyen311090@gmail.com>
2020-03-20 18:11:11 +0700

    change

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,13 +14,6 @@ const TodoList = ({ todos, toggleTodo }) => (
   </ul>
 )
 
-TodoList.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    completed: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired
-  }).isRequired).isRequired,
-  toggleTodo: PropTypes.func.isRequired
-}
+
 
 export default TodoList
